f9c413c40279cd86a467c7d0c80d98a34bdad493
RainbowSci <senpengeden@gmail.com>
2022-06-30 11:13:06 +0800

    Changes

---
+++ models/ResNet.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+# or any of these variants
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet101', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
+
