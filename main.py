0c9b4aad20211e9d63806f26b0824bd2d84014b8
RainbowSci <senpengeden@gmail.com>
2022-06-30 16:10:55 +0800

    Changes

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import models.ResNet as rn
+from evaluation import *
 
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+model = rn.ResNet18().to(device)
+print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+epochs = 5
+
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer, device)
+    test(test_dataloader, model, loss_fn, device)
+print("Done!")
